2887e7805613451b51bf9f45f753bb71bd5e99fe
BenoitDelaforge <benoit.delaforge@neoxio.fr>
2023-10-06 12:56:00 +0200

    Add license file and headers

---
+++ Src/Charts/HorizontalScrollViewer.cs
@@ -0,0 +1,51 @@
+﻿// -----------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2022 Neoxio
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// -----------------------------------------------------------------
+
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Neoxio.Charts
+{
+    /// <summary>
+    /// A <see cref="ScrollViewer"/> that scrolls horizontally when mouse wheel is used.
+    /// </summary>
+    public class HorizontalScrollViewer : ScrollViewer
+    {
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            if (e.Handled) { return; }
+
+            if (ScrollInfo != null && ScrollInfo.ExtentWidth > ScrollInfo.ViewportWidth)
+            {
+                if (e.Delta < 0) { ScrollInfo.MouseWheelRight(); }
+                else { ScrollInfo.MouseWheelLeft(); }
+
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ Src/Charts/TimeLineItemVisualSelector.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2022 Neoxio
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// -----------------------------------------------------------------
+
+using System;
+
+namespace Neoxio.Charts
+{
+    /// <summary>
+    /// Provides a way to customize the visual of items on a <see cref="Timeline"/> control.
+    /// </summary>
+    public abstract class TimeLineItemVisualSelector
+    {
+        /// <summary>
+        /// Creates or retrieves the visual that should be used to display the specified item
+        /// and extracts the date represented by the item.
+        /// </summary>
+        /// <param name="item">Item to display.</param>
+        /// <param name="visual">Visual to use for the specified item.</param>
+        /// <param name="date">Date represented by the specified item.</param>
+        public abstract void GetItemVisual(object item, out object visual, out DateTime? date);
+    }
+}
